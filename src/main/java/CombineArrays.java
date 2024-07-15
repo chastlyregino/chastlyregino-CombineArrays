4d2e481d59c98b0b933be6b414f5ddbfbf36f733
Finn Chastly Manapat <chastly_regino@icloud.com>
2024-07-15 17:20:04 +0000

    combining 2 arrays into 1

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,20 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int arr1Length = arr1.length; // for reusability
+        int arr2Length = arr2.length; // for reusability
+        int combinedArraysLength = arr1Length + arr2Length; // to instantiate the new array's length
 
-        return null;
+        int[] combinedArrays = new int[combinedArraysLength]; // without instantitation of length, for loop will not work
+
+        for (int i = 0; i < arr1Length; i++) {
+            combinedArrays[i] = arr1[i];
+        }
+
+        for (int i = 0; i < arr2Length; i++) {
+            combinedArrays[arr1Length + i] = arr2[i];
+        }
+
+        return combinedArrays;
     }
 }
